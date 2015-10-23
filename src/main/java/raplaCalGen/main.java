3db0759a50666db06bf08a5ce773d177af2765c8
Robin <robin.schlenker10.10@gmail.com>
2015-10-23 19:17:52 +0200

    initial

---
+++ src/main/java/raplaCalGen/main.java
@@ -0,0 +1,18 @@
+package raplaCalGen;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+        
+/**
+ *
+ * @author Robin Schlenker
+ */
+public class main {
+    
+    public static void main(String [ ] args) throws FileNotFoundException, IOException
+    {
+      Interfacer ui = new Interfacer();
+      ui.initUI();
+    }
+}
+
